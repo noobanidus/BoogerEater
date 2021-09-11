275bc4d6840bbd50b007495c6834b2121baa68ff
robotmonkey1000 <robotmonkey000@gmail.com>
2021-09-11 19:14:14 -0400

    Initial Commit Should've did this a while ago

---
+++ src/main/java/studio/robotmonkey1000/boogereater/common/configuration/ModConfigurations.java
@@ -0,0 +1,11 @@
+package studio.robotmonkey1000.boogereater.common.configuration;
+
+public class ModConfigurations {
+
+	public static BoogerConfig BOOGER_CONFIG;
+	
+	public static void setupConfigs() {
+		BOOGER_CONFIG = new BoogerConfig().readConfig();
+	}
+	
+}
